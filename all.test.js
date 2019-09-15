f2b027e73ce88fee997b4f4c401d892d2ec1387b
Dmitriy Vovk <vovk.dmitriy9@gmail.com>
2019-09-15 18:07:26 +0300

    array functions and tests

--- all.test.js
+++ all.test.js
@@ -37,8 +37,30 @@ test('check if 0 is not even', () => {
 
 test('check if "2" is even', () => {
   expect(functions.isEven("2")).toBeTruthy();
-})
+});
 
 test('check if "lol" is not even', () => {
   expect(functions.isEven("lol")).toBeFalsy();
-})
+});
+
+//test arrays
+
+test('check array lenght to equal 3', () => {
+  expect(functions.getArrayLength([1,2,3])).toEqual(3);
+});
+
+test('get first element array to be 1', () => {
+  expect(functions.getFirstElement([1,2,3])).toBe(1);
+});
+
+test('get last element array to be 3', () => {
+  expect(functions.getLastElement([1,2,3])).toBe(3);
+});
+
+test('center element of array to be 2', () => {
+  expect(functions.getCenteredElements([1,2,3])).toBe(2);
+});
+
+test('average sum of centered elements of array to be 2.5', () => {
+  expect(functions.getCenteredElements([1,2,3,4])).toBe(2.5);
+});
--- functions.js
+++ functions.js
@@ -10,8 +10,26 @@ const isEven = x => {
     }
 };
 
+const getArrayLength = x => {
+    return x.length;
+};
 
-module.exports = {isEven, add5};
+const getFirstElement = x => {
+    return x[0];
+}
+
+const getLastElement = x => {
+    return x[x.length - 1];
+}
+
+const getCenteredElements = x => {
+    if(x.length % 2 === 0){
+        return ( x[Math.ceil(x.length / 2 ) -1] + x[Math.ceil(x.length / 2 )] ) /2; 
+    } else {
+        return x[Math.floor(x.length / 2)]
+    }
+}
+module.exports = {isEven, add5, getArrayLength, getFirstElement, getLastElement, getCenteredElements};
 
 
 
